518adacb6e375e43f6be5ff7aec15268a7863cb7
reisistnice <floris10a@gmail.com>
2026-05-11 08:24:01 +0200

    added more mock files

--- Backend/Infrastructure/Persistence/Seed/DbSeeder.cs
+++ Backend/Infrastructure/Persistence/Seed/DbSeeder.cs
@@ -38,7 +38,14 @@ public class DbSeeder : IDbSeeder
             _context.Books.AddRange(
                 new Book { Title = "Clean Code", Author = "Robert C. Martin", ISBN = "978-0132350884" },
                 new Book { Title = "The Pragmatic Programmer", Author = "David Thomas", ISBN = "978-0135957059" },
-                new Book { Title = "Design Patterns", Author = "Gang of Four", ISBN = "978-0201633610" }
+                new Book { Title = "Design Patterns", Author = "Gang of Four", ISBN = "978-0201633610" },
+                new Book { Title = "The Clean Coder", Author = "Robert C. Martin", ISBN = "978-0137081073" },
+                new Book { Title = "Refactoring", Author = "Martin Fowler", ISBN = "978-0134757599" },
+                new Book { Title = "Domain-Driven Design", Author = "Eric Evans", ISBN = "978-0321125217" },
+                new Book { Title = "Introduction to Algorithms", Author = "Thomas H. Cormen", ISBN = "978-0262046305" },
+                new Book { Title = "Code Complete", Author = "Steve McConnell", ISBN = "978-0735619678" },
+                new Book { Title = "You Don't Know JS", Author = "Kyle Simpson", ISBN = "978-1491924464" },
+                new Book { Title = "The Mythical Man-Month", Author = "Frederick P. Brooks Jr.", ISBN = "978-0201835953" }
             );
             await _context.SaveChangesAsync();
         }
